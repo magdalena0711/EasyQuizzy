78cf600d0d9ddf3254ee7992f599051c2c66f7b6
Elena <elenasavic5@gmail.com>
2024-05-10 17:14:34 +0200

    Dodate promene za administratora

---
+++ EasyQuizzy/static/js/deleteUser.js
@@ -0,0 +1,157 @@
+
+
+$(document).ready(function(){
+
+    var clickedRadio = null;
+
+    function successfulRequest(message){
+        var ale = $("#deleteAlert");
+        if (ale.css("display") === "block") {
+            ale.css("display", "none");
+        }
+        var errorDiv = $("#errorMessage");
+        if(errorDiv.css("display") === "none"){
+            errorDiv.css("display", "block");
+            errorDiv.css({
+                "width": "100%",
+                "height": "30%",
+                "display": "flex",
+                "justify-content": "center",
+                "align-items": "center"
+            })
+        }
+        $("#succ").text(message);
+        $("#succ").css({
+            "font-style": "italic",
+            'font-size': "80%",
+            'font-weight': 'bold'
+        });
+        clickedRadio = null;
+        $("#"+username).prop("checked", false);
+        $("#inputUser").attr("disabled", false);
+        setInterval(function(){
+            $("#succ").text("");
+            errorDiv.css("display", "none");
+            
+        },2000);
+    };
+
+    $("#deleteButton").click(function(){
+        var ale = $("#deleteAlert");
+        if (ale.css("display") === "none") {
+          ale.css("display", "block");
+        } else {
+          ale.css("display", "none");
+        }
+    });
+
+    $("#noButton").click(function(){
+        var ale = $("#deleteAlert")
+        if (ale.css("display") === "none") {
+            ale.css("display", "block"); 
+        } else {
+            ale.css("display", "none"); 
+        }
+    });
+
+    $(".userRadios").change(function(){
+        if ($(this).is(':checked')){
+            clickedRadio = $(this);
+        }
+        $("#inputUser").attr("disabled", true);
+    });
+
+    $("#inputUser").on({
+        focus: function(){
+            $(".userRadios").attr("disabled", true);
+        },
+        blur: function(){
+            if($("#inputUser").val() == ''){
+                $(".userRadios").attr("disabled", false);
+            }
+            
+        }
+    });
+
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    const csrftoken = getCookie('csrftoken');
+
+    function get_user(){
+        var username = null;
+        if(clickedRadio != null){
+            username = clickedRadio.attr("id");
+            clickedRadio = null;
+        }else if($("#inputUser").val() != ''){
+            username = $("#inputUser").val();
+        }
+        return username;
+    }
+
+    $("#yesButton").click(function(){
+        msg = "Niste izabrali korisnika koga želite da obrišete";
+        username = get_user();
+        if (username == null){
+            successfulRequest(msg);
+            return;
+        }
+
+        $.ajax({
+            url: '/easyquizzy/delete',
+            method: 'POST',
+            headers:{
+                "X-CSRFToken": csrftoken,
+                "Content-Type": "application/json"
+            },
+            data: {'username': username},
+            success: function(response){
+                successfulRequest(response['message']);
+                if(response['successful'] == true){
+                    $("#"+username).closest('tr').remove();
+                }
+            }
+        })
+
+    });
+
+    $("#moderatorButton").click(function(){
+        msg = "Niste izabrali korisnika koga želite postavite za moderatora";
+        username = get_user();
+        if (username == null){
+            successfulRequest(msg);
+            return;
+        }
+
+        $.ajax({
+            url: '/easyquizzy/addModerator',
+            method: 'POST',
+            headers:{
+                "X-CSRFToken": csrftoken,
+                "Content-Type": "application/json"
+            },
+            data: {'username': username},
+            success: function(response){
+                successfulRequest(response['message']);
+                if(response['successful'] == true){
+                    $("#role"+username).text('moderator');
+                }
+            }
+        })
+    });
+    
+
+    
+});
--- EasyQuizzy/urls.py
+++ EasyQuizzy/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from EasyQuizzy import views
+from EasyQuizzy import views, views_administrator
 
 urlpatterns = [
-    path('', views.statistics, name='statistics')
+    path('', views.statistics, name='statistics'),
+    path('users', views_administrator.show_users, name='show_users'),
+    path('delete', views_administrator.delete_user, name='delete_user'),
+    path('addingQuestions', views_administrator.adding_questions, name='adding_questions'),
+    path('addNewQuestion', views_administrator.add_new_question, name='add_new_question'),
+    path('getQuestionsByCategory', views_administrator.get_questions_category, name='get_questions_by_category'),
+    path('changeQuestion', views_administrator.change_question, name='change_question'),
+    path('addPermittedQuestion', views_administrator.add_permitted_question, name='add_permitted_question'),
+    path('categories', views_administrator.show_categories, name='show_categories'),
+    path('addModerator', views_administrator.add_moderator, name='add_moderator')
 ]
